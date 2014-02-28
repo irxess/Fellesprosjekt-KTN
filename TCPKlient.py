e8d26607c82a2479f70e90b2cc4890bfddee695b
irxess <irxess@gmail.com>
2014-02-28 11:40:55 +0100

    added working multithreading

--- TCPKlient.py
+++ TCPKlient.py
@@ -1,8 +1,8 @@
-import pygame
+#import pygame
 import socket
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(('localhost', 9007))
+connection.connect(('localhost', 9008))
 
 while True:
     connection.send(str(raw_input("skriv noe: ")))
--- TCPServer.py
+++ TCPServer.py
@@ -4,30 +4,53 @@ from threading import Thread
 
 print'SERVER'
 
+class listeningThread (Thread):
+    def __init__(self, threadID, connectionSocket, addr):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.connectionSocket = connectionSocket
+        self.addr = addr
+
+    def run(self):
+        while True: #should include a check if client wants to quit
+            print 'venter pa melding'
+            message, address = self.connectionSocket.recvfrom(1024)
+            # might need a mutex here
+            # docs.python.org/2/library/multiprocessing.html
+            # 16.6.1.4
+            print'package recieved from: ', address, "msg: ", message
+
+
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', 9007))
+serverSocket.bind(('', 9008))
 serverSocket.listen(5)
 
 def acceptClient():
-    
+    threadsSpawned = 0
+    listOfRunningThreads = []
     while True:
         vent=' '
         print 'venter pa klient'
         connectionSocket, addr = serverSocket.accept()
         print 'klient koblet til'
         print'starter ny thread'
-        t = Thread(target=recvMsg(connectionSocket,addr))
-        t.start()
+        #t = Thread(target=recvMsg(connectionSocket,addr))
+        #t.start()
+        listOfRunningThreads.append(listeningThread(threadsSpawned, connectionSocket, addr))
+        listOfRunningThreads[ threadsSpawned ].start()
+        threadsSpawned += 1
         print'etter thread'
 
 
-def recvMsg(connectionSocket,addr):
-    while True:
-        print 'venter pa melding'
-        message, address = connectionSocket.recvfrom(1024)
-        print'package recieved from: ', address, "msg: ", message
+acceptClient()
 
-
-Thread(target=acceptClient()).start()
+#def recvMsg(connectionSocket,addr):
+#    while True:
+#        print 'venter pa melding'
+#        message, address = connectionSocket.recvfrom(1024)
+#        print'package recieved from: ', address, "msg: ", message
+#
+#
+#Thread(target=acceptClient()).start()
 
