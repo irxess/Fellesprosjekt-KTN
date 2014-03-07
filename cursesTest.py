718cfbc12613e0cdaa803d414a7fe38ddf7cfb71
irxess <irxess@gmail.com>
2014-03-07 09:40:06 +0100

    moved files

---
+++ cursesTest.py
@@ -0,0 +1,27 @@
+import curses
+
+curses.initscr()
+             # height, width, begin_y, begin_x
+window = curses.newwin(24, 80, 0, 0)
+window.box()
+window.refresh()
+
+chatwindow = window.derwin(20, 80, 0, 0)
+chatwindow.box()
+chatwindow.addstr(0, 2, "Samtalen", curses.A_BOLD)
+chatwindow.refresh()
+
+inputwindow = window.derwin(4, 80, 20, 0)
+inputwindow.box()
+inputwindow.addstr(0, 2, "Din melding", curses.A_BOLD)
+inputwindow.refresh()
+
+#c = inputwindow.getkey()
+for i in range (5):
+    s = inputwindow.getstr(1,1)
+    chatwindow.addstr(1+i,1,s)
+    chatwindow.refresh()
+
+c = inputwindow.getkey(1,1)
+
+curses.endwin()
