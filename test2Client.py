769cb92d306da84a749ba81bf914f2c98713cb0c
irxess <irxess@gmail.com>
2014-03-10 11:57:56 +0100

    now using asyncore

---
+++ test2Client.py
@@ -0,0 +1,39 @@
+import asyncore, socket, collections
+
+class Client(asyncore.dispatcher):
+    def __init__(self, host_address, name):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = name
+        self.connect(host_address)
+        self.outbox = collections.deque()
+        print 'client started'
+
+    def say(self, message):
+        self.outbox.append(message)
+        print 'enqueued message'
+
+    def handle_write(self):
+        if not self.outbox:
+            return
+        message = self.outbox.popleft()
+        if len(message) > 1024:
+            raise ValueError('message too long')
+        self.send(message)
+
+    def handle_close(self):
+        print 'client: connection closed'
+        self.close()
+
+    def handle_read(self):
+        message = self.recv(1024)
+        if not message:
+            return
+        print 'received ', message
+        #self.send('Hello from client')
+
+name = raw_input('write your nickname: ')
+c = Client(('127.0.0.1', 5007), name)
+c.say("Hello to all other worlds!")
+# spawn other thread reading input here
+asyncore.loop()
---
+++ testServer.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import asyncore, socket, collections
+
+class ClientHandler(asyncore.dispatcher):
+    def __init__(self, host, socket, address):
+        asyncore.dispatcher.__init__(self, socket)
+        self.host = host
+        self.outbox = collections.deque()
+
+    def say(self, message):
+        self.outbox.append(message)
+
+    def handle_read(self):
+        client_message = self.recv(1024)
+        if not client_message:
+            print ('client disconnected?')
+            return
+        print ('broadcasting message')
+        self.host.broadcast(client_message)
+
+    def handle_write(self):
+        if not self.outbox:
+            return
+        message = self.outbox.popleft()
+        if len(message) > 1024:
+            raise ValueError('message too long')
+        self.send(message)
+
+    def handle_close(self):
+        print ("server: closed connection")
+        self.close()
+
+class Server(asyncore.dispatcher):
+    def __init__(self, address=('localhost', 1001)):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(address)
+        self.listen(1)
+        self.remote_clients = []
+        print ("Waiting for connection")
+
+    def handle_accept(self):
+        socket, address = self.accept()
+        print ("connection by", address)
+        self.remote_clients.append(ClientHandler(self, socket, address))
+        socket.send("Hello from server")
+
+    def handle_read(self):
+        print ('received message: %s', self.read())
+
+    def broadcast(self, message):
+        print ('Broadcasting')
+        for client in self.remote_clients:
+            client.say(message)
+
+s = Server(('0.0.0.0', 5007))
+asyncore.loop()
