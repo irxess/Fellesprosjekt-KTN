e2704a29167d4f51b525e9baa6b4fec0f83df187
irxess <irxess@gmail.com>
2014-03-24 11:06:23 +0100

    sjekker brukernavn

--- asynchatClientSend.py
+++ asynchatClientSend.py
@@ -2,7 +2,8 @@ import asynchat
 import asyncore
 import socket
 import threading
- 
+import re
+
 class ChatClient(asynchat.async_chat):
  
     def __init__(self, host, port):
@@ -24,7 +25,12 @@ client = ChatClient('localhost', 5050)
 comm = threading.Thread(target=asyncore.loop)
 comm.daemon = True
 comm.start()
- 
+
+username = raw_input("username: ")
+while ( re.match('^[\w-]+$', username ) is None ):
+    print "Invalid username"
+    username = raw_input("username: ")
+
 while True:
     msg = raw_input('> ')
     client.push(msg + '\n')
